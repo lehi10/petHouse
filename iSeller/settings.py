09a099459adaa505fec13c5fae57fdf5b44b3663
Lehi Qr <lehi_10@hotmail.com>
2019-07-12 02:29:03 -0500

    config

--- iSeller/settings.py
+++ iSeller/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = '$*m@c9$z^2q+)!p!!$x)f1y#a5hisqkvkvr%(-sd!%@07t_or+'
 
 DEBUG = config('DEBUG', default=False, cast=bool)
 
