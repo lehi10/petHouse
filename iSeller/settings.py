8f1d7a17310d9831300867d2afa01b845104fbf8
Lehi Qr <lehi_10@hotmail.com>
2019-07-12 03:07:50 -0500

    config

--- iSeller/settings.py
+++ iSeller/settings.py
@@ -138,15 +138,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATICFILES_DIRS =(os.path.join(BASE_DIR,'static'),)
-
-
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-
+STATICFILES_DIRS =(os.path.join(BASE_DIR,'static'),)
 
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 MEDIA_URL = '/media/'
- 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
